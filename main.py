5b40db020ec80c73eb487bcc61470600fd86433b
Szymon Bartol <szymon.bartol@interia.pl>
2022-05-25 21:14:58 +0200

    edycja

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import math
 #n - pierwsza zmienna
 #k - druga zmienna
 
-n = int(input("Podaj liczbę n (w liczniku): "))
-k = int(input("Podaj liczbę k (w mianowniku): "))
+n = int(input())
+k = int(input())
 
 if  k==1 or k == n:
     print(1)
