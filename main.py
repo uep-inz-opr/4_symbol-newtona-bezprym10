6b9bab86fbfd2f26c604709bc47e409332c7673f
Szymon Bartol <szymon.bartol@interia.pl>
2022-05-25 21:20:54 +0200

    dodanie zmiennej pomocniczej

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import math
 #n - pierwsza zmienna
 #k - druga zmienna
 
-n = int(input())
-k = int(input())
+n = int(input("Podaj liczbę n (w liczniku): "))
+k = int(input("Podaj liczbę k (w mianowniku): "))
 
-if  k==1 or k == n:
+if k == 1 or k == n:
     print(1)
 
 if k > n:
@@ -15,5 +15,7 @@ if k > n:
 else:
     x = math.factorial(n)
     y = math.factorial(k)
-    symbolNewtona = x // (y*(n-k))
+    z = math.factorial(n-k)
+    symbolNewtona = x // (y*z)
     print(symbolNewtona)
+
