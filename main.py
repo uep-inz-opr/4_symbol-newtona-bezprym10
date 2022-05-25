4fd310f230ecf20a6b4c950b3744ae73a4f337cf
Szymon Bartol <szymon.bartol@interia.pl>
2022-05-25 21:23:34 +0200

    usunięcie zbędnych elementów opisowych

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import math
 
-#Celem programu jest obliczanie symbolu Newtona z dwóch zmiennych (n,k)
-#n - pierwsza zmienna
-#k - druga zmienna
-
-n = int(input("Podaj liczbę n (w liczniku): "))
-k = int(input("Podaj liczbę k (w mianowniku): "))
+n = int(input())
+k = int(input())
 
 if k == 1 or k == n:
     print(1)
